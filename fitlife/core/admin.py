9cb1e92b2cb90ec0563893f055fedf256e85a9ec
gabrielloliveira <gabrielloliveira097@gmail.com>
2022-08-02 19:29:49 -0300

    feat: add user model

--- fitlife/core/admin.py
+++ fitlife/core/admin.py
@@ -1,3 +1,90 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from fitlife.core.models import User
+
+
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+
+    password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_("Password confirmation"), widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("name", "email")
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+
+    password = ReadOnlyPasswordHashField(
+        label=_("Senha"),
+        help_text=_("Você pode alterar a senha " f"<a href='../password/'>aqui</a>."),
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            "name",
+            "email",
+            "password",
+            "type",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+        ]
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = ("name", "email", "type", "is_superuser")
+    list_filter = ("type",)
+    fieldsets = (
+        (None, {"fields": ("name", "email", "password")}),
+        ("Permissions", {"fields": ("type", "is_active", "is_staff", "is_superuser")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("name", "email", "password1", "password2", "type", "is_staff", "is_superuser"),
+            },
+        ),
+    )
+    search_fields = ("name", "email")
+    ordering = ("name",)
+    filter_horizontal = ()
--- fitlife/core/models.py
+++ fitlife/core/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from fitlife.core.managers import UserManager
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(_("criado em"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("atualizado em"), auto_now_add=True)
+    uuid = models.UUIDField(_("UUID"), editable=False, unique=True, default=uuid.uuid4)
+
+    class Meta:
+        abstract = True
+
+
+class User(AbstractBaseUser, PermissionsMixin, BaseModel):
+    TYPE_STUDENT = "student"
+    TYPE_OWNER = "owner"
+    TYPE_TRAINER = "trainer"
+    TYPE_CHOICES = (
+        (TYPE_TRAINER, _("Treinador")),
+        (TYPE_STUDENT, _("Aluno")),
+        (TYPE_OWNER, _("Gestor")),
+    )
+
+    email = models.EmailField(_("endereço de email"), unique=True)
+    name = models.CharField(_("nome"), max_length=255)
+    is_active = models.BooleanField(_("usuário está ativo?"), default=True)
+    is_staff = models.BooleanField(_("é um membro da equipe?"), default=False)
+    type = models.CharField(_("tipo de usuário"), max_length=20, choices=TYPE_CHOICES)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
