d5e2c62074d3237c458da0f9468dc645a586589a
gabrielloliveira <gabrielloliveira097@gmail.com>
2022-08-16 22:37:03 -0300

    feat: add admin choices

--- fitlife/core/forms.py
+++ fitlife/core/forms.py
@@ -1,10 +1,18 @@
 from django import forms
 
 from fitlife.core.models import User
+from django.utils.translation import gettext_lazy as _
+
+
+ADMIN_CHOICES = (
+    (User.TYPE_TRAINER, _("Treinador")),
+    (User.TYPE_OWNER, _("Gestor")),
+)
 
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(label="Senha", required=False)
+    type = forms.TypedChoiceField(choices=ADMIN_CHOICES)
 
     class Meta:
         model = User
@@ -16,8 +24,18 @@ class UserForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = super(UserForm, self).save(commit=False)
-        if self.data["password"]:
-            user.set_password(self.data["password"])
+        if self.cleaned_data["password"]:
+            user.set_password(self.cleaned_data["password"])
         user.save()
         user.refresh_from_db()
         return user
+
+
+class StudentForm(UserForm):
+    class Meta:
+        model = User
+        fields = ("name", "email", "password")
+
+    def save(self, commit=True):
+        self.cleaned_data["type"] = User.TYPE_TRAINER
+        return super(StudentForm, self).save(commit=commit)
