07288b3b0390b7e5b0f5cc3e0b9b2ccfff0924e8
gabrielloliveira <gabrielloliveira097@gmail.com>
2022-08-23 21:02:52 -0300

    feat: add delete practice

--- fitlife/practice/urls.py
+++ fitlife/practice/urls.py
@@ -6,4 +6,5 @@ app_name = "practice"
 urlpatterns = [
     path("", views.list_practice, name="list"),
     path("adicionar/", views.add_practice, name="create"),
+    path("<uuid:uuid>/deletar/", views.delete_practice, name="delete"),
 ]
--- fitlife/practice/views.py
+++ fitlife/practice/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views.decorators.http import require_POST
 
@@ -28,4 +28,13 @@ def add_practice(request):
     else:
         print(form.errors)
         messages.error(request, "Erro ao cadastrar treino.", extra_tags="danger")
+    return HttpResponseRedirect(reverse("practice:list"))
+
+
+@require_POST
+@login_required
+def delete_practice(request, uuid):
+    practice = get_object_or_404(Practice, uuid=uuid)
+    practice.delete()
+    messages.success(request, "Treino deletado com sucesso.")
     return HttpResponseRedirect(reverse("practice:list"))
