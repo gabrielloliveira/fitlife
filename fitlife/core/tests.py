63f81d19377859421cd0b60feb7150cc46241a22
lucasbezerra26 <lucasbezerra527@gmail.com>
2022-09-13 23:36:58 -0300

    feat: test

--- fitlife/core/tests.py
+++ fitlife/core/tests.py
@@ -1,3 +1,56 @@
+from fitlife.core.models import User
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+
+from fitlife.practice.models import Practice
+
 
 # Create your tests here.
+
+
+class Alunos(TestCase):
+    @classmethod
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email="teste@teste.com",
+            name="test",
+            type="student",
+            password="test",
+        )
+        self.client = Client()
+
+    def test_meu_treino(self):
+        practice = Practice.objects.create(
+            name="Treino B",
+        )
+        practice.users.add(self.user)
+        practice.save()
+        self.client.login(username="teste@teste.com", password="test")
+        response = self.client.get("/meu-treino/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_frequency_inicar(self):
+        self.client.login(username="teste@teste.com", password="test")
+        practice = Practice.objects.create(
+            name="Treino B",
+        )
+        practice.users.add(self.user)
+        practice.save()
+        response = self.client.post("/treinos/frequencia/iniciar/", follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_frequency_finalizar(self):
+        self.client.login(username="teste@teste.com", password="test")
+        practice = Practice.objects.create(
+            name="Treino B",
+        )
+        practice.users.add(self.user)
+        practice.save()
+        response = self.client.post("/treinos/frequencia/finalizar/", follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    # def test_alunos(self):
+    #     self.client.login(username="teste@teste.com", password="test")
+    #     response = self.client.get("/alunos/")
+    #     self.assertEqual(response.status_code, 200)
--- fitlife/practice/tests.py
+++ fitlife/practice/tests.py
@@ -1,3 +1,39 @@
+from fitlife.core.models import User
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
 
-# Create your tests here.
+from fitlife.practice.models import Practice
+
+
+class Admin(TestCase):
+    @classmethod
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email="teste1@teste.com",
+            name="test",
+            type=User.TYPE_TRAINER,
+            is_staff=True,
+            password="test",
+        )
+        self.client = Client()
+
+    def test_get_alunos(self):
+        self.client.login(username="teste1@teste.com", password="test")
+        response = self.client.get("/alunos/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_alunos_add(self):
+        self.client.login(username="teste1@teste.com", password="test")
+        response = self.client.post("/alunos/adicionar/", follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_equipes(self):
+        self.client.login(username="teste1@teste.com", password="test")
+        response = self.client.get("/equipe/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_equipe_add(self):
+        self.client.login(username="teste1@teste.com", password="test")
+        response = self.client.post("/equipe/adicionar/", follow=True)
+        self.assertEqual(response.status_code, 200)
